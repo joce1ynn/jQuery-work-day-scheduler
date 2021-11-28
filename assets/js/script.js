e0ddbfe123ae1c6751b66717fa3efa483787a038
Jocelyn Tian <jocelyntianww@gmail.com>
2021-11-28 00:12:45 -0700

    add ability to show saved event

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,35 @@ $("#currentDay").text(moment().format("dddd, MMM Do YYYY"));
 
 // 2. color-coded time block showing past, present, and future
 // 2.1 refresh page every minutes in case users leave window on for hours
-setInterval(function () {
-  var currentHour = moment().hours(); //shows in 24 hous
 
-  $("textarea").each(function () {
-    var textAreaHour = $(this).attr("id").split("-")[1];
+var currentHour = moment().hours(); //shows in 24 hous
 
-    if (textAreaHour < currentHour) {
-      $(this).addClass("past"); 
-    } else if (textAreaHour == currentHour) {
-      $(this).addClass("present"); 
-    } else {
-      $(this).addClass("future"); 
-    }
-  });
-}, 60000);
+$("textarea").each(function () {
+  var textAreaHour = $(this).attr("id").split("-")[1];
 
-// // save event in local storage
-// $("button").on("click", function () {
-//   var textInput = $("textarea").text();
-//   localStorage.setItem("savedText", JSON.stringify(textInput));
-// });
+  if (textAreaHour < currentHour) {
+    $(this).addClass("past");
+  } else if (textAreaHour == currentHour) {
+    $(this).addClass("present");
+  } else {
+    $(this).addClass("future");
+  }
+});
 
+// 3. save event in local storage
+$("button").on("click", function () {
+  var time = $(this).siblings("textarea").attr("id");
+  var textInput = $(this).siblings("textarea").val();
 
-// //
+  localStorage.setItem(time, textInput);
+});
+
+// 4. present saved event when refresh
+$(".time-block").each(function () {
+  var id = $(this).children("textarea").attr("id");
+  var savedText = localStorage.getItem(id);
+
+  if (savedText !== null) {
+    $(this).children("textarea").val(savedText);
+  }
+});
