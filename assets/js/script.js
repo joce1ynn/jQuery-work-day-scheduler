2009a0b6c224d413136849926cec10a5e2a8b12e
Jocelyn Tian <jocelyntianww@gmail.com>
2021-11-27 17:24:34 -0700

    add current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
-// add current date in header 
+// add current date in header
+$("#currentDay").text(moment().format("dddd, MMM Do YYYY"));
 
+// add time blocks with standard business hours
+
+// color-coded time block showing past, present, and future
+
+// enter an event into time block
+
+// save event in local storage
