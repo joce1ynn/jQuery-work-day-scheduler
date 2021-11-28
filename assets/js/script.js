49583cf1d80c6a5201c38c90906e4ac0c5151dab
Jocelyn Tian <jocelyntianww@gmail.com>
2021-11-27 21:38:38 -0700

    color coded time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,29 @@
-// add current date in header
+// 1. add current date in header
 $("#currentDay").text(moment().format("dddd, MMM Do YYYY"));
 
+// 2. color-coded time block showing past, present, and future
+// 2.1 refresh page every minutes in case users leave window on for hours
+setInterval(function () {
+  var currentHour = moment().hours(); //shows in 24 hous
+
+  $("textarea").each(function () {
+    var textAreaHour = $(this).attr("id").split("-")[1];
+
+    if (textAreaHour < currentHour) {
+      $(this).addClass("past"); 
+    } else if (textAreaHour == currentHour) {
+      $(this).addClass("present"); 
+    } else {
+      $(this).addClass("future"); 
+    }
+  });
+}, 60000);
+
+// // save event in local storage
+// $("button").on("click", function () {
+//   var textInput = $("textarea").text();
+//   localStorage.setItem("savedText", JSON.stringify(textInput));
+// });
 
 
-// color-coded time block showing past, present, and future
-
-// enter an event into time block
-
-// save event in local storage
+// //
